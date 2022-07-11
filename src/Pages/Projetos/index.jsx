40876613a6920e8631060c0d879a1d3b41eb850e
Gabriel <gabrielbvlkenzie@gmail.com>
2022-07-11 08:25:53 -0300

    feat: projects

--- src/Pages/Projetos/index.jsx
+++ src/Pages/Projetos/index.jsx
@@ -1,10 +1,140 @@
 import { useNavigate } from "react-router-dom";
 import Particle from "../../Components/Background-Animation/particles";
+import projectsLogo from "../../Assets/imgs/projects.png";
+import projectBanner1 from "../../Assets/imgs/project1.png";
+import projectBanner2 from "../../Assets/imgs/project2.png";
+import projectBanner3 from "../../Assets/imgs/project3.png";
+import projectBanner4 from "../../Assets/imgs/project4.png";
+import projectBanner5 from "../../Assets/imgs/project5.png";
+import projectBanner6 from "../../Assets/imgs/project6.png";
+import projectBanner7 from "../../Assets/imgs/project7.png";
+import projectBanner8 from "../../Assets/imgs/project8.png";
+import projectBanner9 from "../../Assets/imgs/project9.png";
+import projectBanner10 from "../../Assets/imgs/project10.png";
+import "../../Styles/projects.css";
 
 function Projects() {
     return (
         <div className="projects_container">
             <Particle />
+
+            <div className="img_projects">
+                <h1>Gostaria de conhecer alguns projetos criados por mim? É só descer!</h1>
+                <img src={projectsLogo} alt="img_projects" />
+            </div>
+
+            <section className="projects_container_internal">
+                <div className="project">
+                    <img src={projectBanner1} alt="img_projects" />
+                    <div className="project_description">
+                        <p>Projeto:</p>
+                        <p>Descrição:</p>
+                    </div>
+                    <div className="project_buttons">
+                        <button>Repositório</button>
+                        <button>Projeto</button>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src={projectBanner2} alt="img_projects" />
+                    <div className="project_description">
+                        <p>Projeto:</p>
+                        <p>Descrição:</p>
+                    </div>
+                    <div className="project_buttons">
+                        <button>Repositório</button>
+                        <button>Projeto</button>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src={projectBanner3} alt="img_projects" />
+                    <div className="project_description">
+                        <p>Projeto:</p>
+                        <p>Descrição:</p>
+                    </div>
+                    <div className="project_buttons">
+                        <button>Repositório</button>
+                        <button>Projeto</button>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src={projectBanner4} alt="img_projects" />
+                    <div className="project_description">
+                        <p>Projeto:</p>
+                        <p>Descrição:</p>
+                    </div>
+                    <div className="project_buttons">
+                        <button>Repositório</button>
+                        <button>Projeto</button>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src={projectBanner5} alt="img_projects" />
+                    <div className="project_description">
+                        <p>Projeto:</p>
+                        <p>Descrição:</p>
+                    </div>
+                    <div className="project_buttons">
+                        <button>Repositório</button>
+                        <button>Projeto</button>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src={projectBanner6} alt="img_projects" />
+                    <div className="project_description">
+                        <p>Projeto:</p>
+                        <p>Descrição:</p>
+                    </div>
+                    <div className="project_buttons">
+                        <button>Repositório</button>
+                        <button>Projeto</button>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src={projectBanner7} alt="img_projects" />
+                    <div className="project_description">
+                        <p>Projeto:</p>
+                        <p>Descrição:</p>
+                    </div>
+                    <div className="project_buttons">
+                        <button>Repositório</button>
+                        <button>Projeto</button>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src={projectBanner8} alt="img_projects" />
+                    <div className="project_description">
+                        <p>Projeto:</p>
+                        <p>Descrição:</p>
+                    </div>
+                    <div className="project_buttons">
+                        <button>Repositório</button>
+                        <button>Projeto</button>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src={projectBanner9} alt="img_projects" />
+                    <div className="project_description">
+                        <p>Projeto:</p>
+                        <p>Descrição:</p>
+                    </div>
+                    <div className="project_buttons">
+                        <button>Repositório</button>
+                        <button>Projeto</button>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src={projectBanner10} alt="img_projects" />
+                    <div className="project_description">
+                        <p>Projeto:</p>
+                        <p>Descrição:</p>
+                    </div>
+                    <div className="project_buttons">
+                        <button>Repositório</button>
+                        <button>Projeto</button>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
--- src/Pages/Sobre/index.jsx
+++ src/Pages/Sobre/index.jsx
@@ -16,7 +16,7 @@ function About() {
             <Particle />
 
             <div className="img_about">
-                <h1>Sobre Mim</h1>
+                <h1>Conheça um pouco mais sobre mim!</h1>
                 <img src={aboutLogo} alt="img_about" />
             </div>
             {/* <div className="typewriter">
@@ -24,12 +24,7 @@ function About() {
                     <Typewriter
                         options={{
                             strings: [
-                                "Algumas curiosidades que vocês verão abaixo...",
-                                "Sou profissional de educação fisica por formação.",
-                                "Sou faixa azul de jiu-jitsu.",
-                                "Já trabalhei como desenvolvedor front-end.",
-                                "Já ouvi que não confiavam no meu trabalho.",
-                                "Tenho uma meta como dev e irei chegar la.",
+                                "Olá, me chamo Gabriel Bezerra e irei apresentar minhas tecnologias e alguns fatos sobre mim antes e depois da programação.",
                             ],
                             autoStart: true,
                             loop: true,
@@ -46,7 +41,7 @@ function About() {
                 </h1>
 
                 <section className="techs">
-                    <div className="techs_internal">
+                    <div className="techs_internal" id="techs_internal1">
                         <div className="tech">
                             <img src={html} alt="img_about" />
                         </div>
@@ -71,77 +66,85 @@ function About() {
                 </section>
 
                 <section className="about_no_tech">
-                    <h2>
-                        Alguns fatos sobre mim que <b className="blue_light">não se relacionam</b> a
-                        programação...
-                    </h2>
-                    <p className="about_description">
-                        1 - Sou{" "}
-                        <b className="blue_light">bacharel e licenciado em Educação Física</b> desde
-                        2017.
-                    </p>
-                    <p className="about_description">
-                        2 - Atuei como <b className="blue_light">personal trainer</b> entre 2013 e
-                        2021, durante 8 anos, parando apenas por conta da pandemia.
-                    </p>
-                    <p className="about_description">
-                        3 - <b className="blue_light">Empreendo</b> desde cedo, meu{" "}
-                        <b className="blue_light">maior sucesso</b> foi uma loja de produtos
-                        personalizados feitos por mim, desde a criação da arte até a confecção de
-                        produtos, a <b className="blue_light">Buttons Store</b> esteve ativa de 2018
-                        até 2022.
-                    </p>
-                    <p className="about_description">
-                        4 - Sou entusiasta das lutas,{" "}
-                        <b className="blue_light">faixa azul de jiu-jitsu</b> e venho treinando,
-                        além dele, o <b className="blue_light">muay thai</b> desde 2012.
-                    </p>
+                    <div className="title_about_description">
+                        <h2>
+                            Alguns fatos sobre mim que{" "}
+                            <b className="blue_light">não se relacionam</b> a programação...
+                        </h2>
+                    </div>
+                    <div className="body_about_description">
+                        <p className="about_description">
+                            Sou{" "}
+                            <b className="blue_light">bacharel e licenciado em Educação Física</b>{" "}
+                            desde 2017.
+                        </p>
+                        <p className="about_description">
+                            Atuei como <b className="blue_light">personal trainer</b> entre 2013 e
+                            2021, durante 8 anos, parando apenas por conta da pandemia.
+                        </p>
+                        <p className="about_description">
+                            <b className="blue_light">Empreendo</b> desde cedo, meu{" "}
+                            <b className="blue_light">maior sucesso</b> foi uma loja de produtos
+                            personalizados feitos por mim, desde a criação da arte até a confecção
+                            de produtos, a <b className="blue_light">Buttons Store</b> esteve ativa
+                            de 2018 até 2022.
+                        </p>
+                        <p className="about_description">
+                            Sou entusiasta das lutas,{" "}
+                            <b className="blue_light">faixa azul de jiu-jitsu</b> e venho treinando,
+                            além dele, o <b className="blue_light">muay thai</b> desde 2012.
+                        </p>
+                    </div>
                 </section>
                 <div className="about_tech">
-                    <h2>
-                        Alguns fatos sobre mim <b className="blue_light">relacionados</b> a
-                        programação...
-                    </h2>
-                    <p className="about_description">
-                        1 - Fiz o curso da <b className="blue_light">Kenzie Academy Brasil</b> de
-                        programação full stack, durante 1 ano,{" "}
-                        <b className="blue_light">2 mil horas</b>.
-                    </p>
-                    <p className="about_description">
-                        2 - Apesar de ter estudado front e back, minha{" "}
-                        <b className="blue_light">paixão</b> é pelo{" "}
-                        <b className="blue_light">front</b>, tanto na programação quanto no designer
-                        de paginas e produtos.
-                    </p>
-                    <p className="about_description">
-                        3 - Encerrei meu empreendimento pois{" "}
-                        <b className="blue_light">consegui um trabalho</b> como programador
-                        front-end em 2022, entretanto, não deu certo, mas não voltei com a loja,
-                        continuo me <b className="blue_light">especializando</b> e buscando um{" "}
-                        <b className="blue_light">novo trabalho</b>.
-                    </p>
-                    <p className="about_description">
-                        4 - Com 2 semanas de trabalho ouvi essa frase{" "}
-                        <b className="red_light">"Eu não confio no seu trabalho."</b>. 🙂
-                    </p>
-                    <p className="about_description">
-                        5 - Acho que o segredo de se vencer na vida é{" "}
-                        <b className="blue_light">compreender a dor de agora</b>, ninguem nasceu no{" "}
-                        <b className="blue_light">topo da montanha</b>, continuei estudando, decidi
-                        abrir mão do back pelo menos por enquanto e{" "}
-                        <b className="blue_light">focar 100% no front</b>.
-                    </p>
-                    <p className="about_description">
-                        6 - Minha maior vontade é um dia chegar na posição de{" "}
-                        <b className="blue_light">liderar/treinar</b> pessoas e dizer para elas
-                        todos os dias{" "}
-                        <b className="blue_light">
-                            "Eu confio no seu trabalho, qualquer dúvida me procure, iremos aprender
-                            juntos!"
-                        </b>{" "}
-                        pois um dia ouvi o contrario e espero
-                        <b className="blue_light">nunca</b> fazer o mesmo.
-                    </p>
+                    <div className="title_about_description">
+                        <h2>
+                            Alguns fatos sobre mim <b className="blue_light">relacionados</b> a
+                            programação...
+                        </h2>
+                    </div>
+                    <div className="body_about_description">
+                        <p className="about_description">
+                            Fiz o curso da <b className="blue_light">Kenzie Academy Brasil</b> de
+                            programação full stack, durante 1 ano,{" "}
+                            <b className="blue_light">2 mil horas</b>.
+                        </p>
+                        <p className="about_description">
+                            Apesar de ter estudado front e back, minha{" "}
+                            <b className="blue_light">paixão</b> é pelo{" "}
+                            <b className="blue_light">front</b>, tanto na programação quanto no
+                            designer de paginas e produtos.
+                        </p>
+                        <p className="about_description">
+                            Encerrei meu empreendimento pois{" "}
+                            <b className="blue_light">consegui um trabalho</b> como programador
+                            front-end em 2022, entretanto, não deu certo, mas não voltei com a loja,
+                            continuo me <b className="blue_light">especializando</b> e buscando um{" "}
+                            <b className="blue_light">novo trabalho</b>.
+                        </p>
+                        <p className="about_description">
+                            Com 2 semanas de trabalho ouvi essa frase{" "}
+                            <b className="red_light">"Eu não confio no seu trabalho."</b>. 🙂
+                        </p>
+                        <p className="about_description">
+                            Acho que o segredo de se vencer na vida é{" "}
+                            <b className="blue_light">compreender a dor de agora</b>, ninguem nasceu
+                            no <b className="blue_light">topo da montanha</b>, continuei estudando,
+                            decidi abrir mão do back pelo menos por enquanto e{" "}
+                            <b className="blue_light">focar 100% no front</b>.
+                        </p>
+                        <p className="about_description">
+                            Minha maior vontade é um dia chegar na posição de{" "}
+                            <b className="blue_light">liderar/treinar</b> pessoas e dizer para elas
+                            todos os dias{" "}
+                            <b className="blue_light">
+                                "Eu confio no seu trabalho, qualquer dúvida me procure, iremos
+                                aprender juntos!"
+                            </b>{" "}
+                            pois um dia ouvi o contrario e espero
+                            <b className="blue_light">nunca</b> fazer o mesmo.
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
